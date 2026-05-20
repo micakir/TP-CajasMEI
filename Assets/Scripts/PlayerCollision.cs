f6c70ba845fa7cc468b2bd6e636216545899141a
micakir <micaakir@gmail.com>
2026-05-19 22:10:55 -0300

    el timer

--- Assets/Scripts/PlayerCollision.cs
+++ Assets/Scripts/PlayerCollision.cs
@@ -4,11 +4,14 @@ using UnityEngine;
 
 public class PlayerCollision : MonoBehaviour
 {
-  
+  public Timer timer;
+
     void OnCollisionEnter(Collision col)
     {
         if(col.gameObject.CompareTag("Caja"))
         {
+            timer.juegoTerminado = true;
+            col.gameObject.GetComponent<CajaMovement>().enabled = false;
             Destroy(gameObject);
         }
     }
---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Timer : MonoBehaviour
+{
+    public TextMeshProUGUI txt_Timer;
+
+    float tiempo;
+
+    public bool juegoTerminado = false;
+
+    void Update()
+    {
+        if(juegoTerminado == false)
+        {
+            tiempo += Time.deltaTime;
+
+            txt_Timer.text = "Tiempo: " + tiempo.ToString("F2");
+        }
+    }
+}
